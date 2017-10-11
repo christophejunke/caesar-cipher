45b7dbd86e9f576cba62b0b2c327873e29370333
Fabien Lheureux <fabien.lheureux@trust-in-soft.com>
2017-10-11 16:38:14 +0200

    Check all strings of length <= len

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ void gen_test(int user_shift, int len, char *str)
     res1 = encrypt(str, len, user_shift);
     printf("Result: %s\n", res1);
 
-    printf("Decrypt text '%s'\n", res1);
-    res2 = decrypt(res1, len, user_shift);
+    printf("Decrypt text '%s'\n", str);
+    res2 = decrypt(str, len, user_shift);
     printf("Result: %s\n", res2);
 
     free(res1);
@@ -34,6 +34,10 @@ int main(void)
     printf("\nTest 3: Shift with all possible input\n");
     int user_shift;
     tis_make_unknown((char *)&user_shift, sizeof(int));
+    gen_test(user_shift, len, str);
+
+    printf("\nTest 4: All possible input\n");
+    tis_make_unknown(str, len - 1);
     gen_test(user_shift, len, str);
 
     return 0;
