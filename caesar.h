618386a5002692bfb7ed1db9577b15b94126e4aa
Fabien Lheureux <fabien.lheureux@trust-in-soft.com>
2017-10-11 12:41:20 +0200

    Initial commit

---
+++ caesar.h
@@ -0,0 +1,15 @@
+void check_magic_number(void);
+
+/* \brief Encrypt a string with a right shift specified by the user
+   \param [in] str The string to encrypt
+   \param [in] d The right shift to perform
+   \return The pointer to the encrypted string
+*/
+char *encrypt(char *str, int d);
+
+/* \brief Decrypt a string with a left shift specified by the user
+   \param [in] str The string to decrypt
+   \param [in] d The left shift to perform
+   \return The pointer to the decrypted string
+*/
+char *decrypt(char *str, int d);
