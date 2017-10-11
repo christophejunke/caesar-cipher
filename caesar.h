bcb6f4eb1581f5890ec79e81838743c85ec77bd9
Fabien Lheureux <fabien.lheureux@trust-in-soft.com>
2017-10-11 12:42:10 +0200

    Fix allocation

--- caesar.h
+++ caesar.h
@@ -2,14 +2,16 @@ void check_magic_number(void);
 
 /* \brief Encrypt a string with a right shift specified by the user
    \param [in] str The string to encrypt
+   \param [in] len The length of the string str
    \param [in] d The right shift to perform
    \return The pointer to the encrypted string
 */
-char *encrypt(char *str, int d);
+char *encrypt(char *str, int len, int d);
 
 /* \brief Decrypt a string with a left shift specified by the user
    \param [in] str The string to decrypt
+   \param [in] len The length of the string str
    \param [in] d The left shift to perform
    \return The pointer to the decrypted string
 */
-char *decrypt(char *str, int d);
+char *decrypt(char *str, int len, int d);
